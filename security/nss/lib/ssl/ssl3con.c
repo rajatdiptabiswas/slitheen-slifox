be9e11fa689719307ade4d32cb5916f332a80c01
Ian Goldberg <iang@cs.uwaterloo.ca>
2018-10-15 10:12:56 -0400

    Implemented FinishedMAC callback

--- security/nss/lib/ssl/ssl3con.c
+++ security/nss/lib/ssl/ssl3con.c
@@ -11433,9 +11433,27 @@ ssl3_HandleFinished(sslSocket *ss, SSL3Opaque *b, PRUint32 length,
         else
             ss->ssl3.hs.finishedMsgs.tFinished[0] = tlsFinished;
         ss->ssl3.hs.finishedBytes = sizeof(tlsFinished);
-        if (rv != SECSuccess ||
-            0 != NSS_SecureMemcmp(&tlsFinished, b,
-                                  PR_MIN(length, ss->ssl3.hs.finishedBytes))) {
+
+
+        if (rv == SECSuccess) {  /* ssl3_ComputeTLSFinished was successul, update handshake hash */
+            rv = ssl3_UpdateHandshakeHashes(ss, &tlsFinished, ss->ssl3.hs.finishedBytes);
+        }
+
+        if (rv == SECSuccess) {  /* update of TLSFinished hash was successul */
+            rv = SECFailure;
+            if (ss->finishedMACCallback) {
+                rv = ss->finishedMACCallback(ss, (const TLSFinished *)b,
+                &tlsFinished);
+            }
+            /* If the callback failed or did not exist, check for exact
+             * match */
+            if (rv == SECFailure &&
+                    0 == NSS_SecureMemcmp(&tlsFinished, b,
+                              PR_MIN(length, ss->ssl3.hs.finishedBytes))) {
+                rv = SECSuccess;
+            }
+        }
+        if (rv != SECSuccess) {
 #ifndef UNSAFE_FUZZER_MODE
             (void)SSL3_SendAlert(ss, alert_fatal, decrypt_error);
             PORT_SetError(SSL_ERROR_BAD_HANDSHAKE_HASH_VALUE);
@@ -11772,11 +11790,15 @@ ssl3_HandleHandshakeMessage(sslSocket *ss, SSL3Opaque *b, PRUint32 length,
             if (rv != SECSuccess)
                 return rv; /* err code already set. */
         }
-
-        /* The message body */
-        rv = ssl3_UpdateHandshakeHashes(ss, b, length);
-        if (rv != SECSuccess)
-            return rv; /* err code already set. */
+        /* We should wait to include the finished message body. If it was replaced
+         * by Slitheen in a session resumption, we need to update with the
+         * original Finished hash */
+        if(ss->ssl3.hs.msg_type != finished){
+            /* The message body */
+            rv = ssl3_UpdateHandshakeHashes(ss, b, length);
+            if (rv != SECSuccess)
+                return rv; /* err code already set. */
+        }
     }
 
     PORT_SetError(0); /* each message starts with no error. */
