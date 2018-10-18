add8020b2a9876e0e9c4c0e76895dadfb50775b1
Ian Goldberg <iang@cs.uwaterloo.ca>
2018-10-18 12:53:59 -0400

    Added SlitheenConnector to communicate with SOCKS proxy

--- netwerk/protocol/http/nsHttpConnectionMgr.h
+++ netwerk/protocol/http/nsHttpConnectionMgr.h
@@ -20,6 +20,7 @@
 #include "ARefBase.h"
 #include "nsWeakReference.h"
 #include "TCPFastOpen.h"
+#include "nsHttpSlitheenConnector.h"
 
 #include "nsINamed.h"
 #include "nsIObserver.h"
@@ -801,6 +802,9 @@ class nsHttpConnectionMgr final : public nsIObserver, public AltSvcCache {
   // Then, it notifies selected transactions' connection of the new active tab
   // id.
   void NotifyConnectionOfWindowIdChange(uint64_t previousWindowId);
+
+  // The Slitheen Connector
+  nsAutoPtr<nsHttpSlitheenConnector> mSlitheenConnector;
 };
 
 NS_DEFINE_STATIC_IID_ACCESSOR(nsHttpConnectionMgr::nsHalfOpenSocket,
