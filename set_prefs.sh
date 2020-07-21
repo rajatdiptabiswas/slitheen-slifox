6eac2ad7dd6e6c344aa70ef38937cadb5dec4a6d
Cecylia Bocovich <cohosh@torproject.org>
2020-07-21 14:04:45 -0400

    Update default prefences to disable HTTP/2.0

--- set_prefs.sh
+++ set_prefs.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
 
-echo "user_pref(\"security.tls.version.fallback-limit\", 3);\nuser_pref(\"security.tls.version.max\", 3);" >> obj-x86_64-pc-linux-gnu/tmp/profile-default/prefs.js
+# Disable TLS 1.3
+echo "user_pref(\"security.tls.version.fallback-limit\", 3);\nuser_pref(\"security.tls.version.max\", 3);\n" >> obj-x86_64-pc-linux-gnu/tmp/profile-default/prefs.js
+
+#Disable HTTP/2.0
+echo "user_pref(\"network.http.spdy.enabled.http2\", false);\n" >> obj-x86_64-pc-linux-gnu/tmp/profile-default/prefs.js
